b8981a69a76891432373114f24a9180be1f6c794
Melinda Golden <info@melindagolden.com>
2024-12-15 10:57:28 -0500

    feat: refactor the BaseCard to be a compound component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import BaseCard from "./components/base/BaseCard";
 
 function App() {
   return (
-    <div className="mx-auto max-w-7xl p-4 sm:p-6 lg:p-8">
+    <main className="mx-auto max-w-7xl p-4 sm:p-6 lg:p-8">
       <div className="mx-auto max-w-3xl">
         <BaseCard>
-          <p>Hello world!</p>
+          <BaseCard.Content>
+            <p>Hello world!</p>
+          </BaseCard.Content>
         </BaseCard>
       </div>
-    </div>
+    </main>
   );
 }
 
--- src/components/base/BaseCard.tsx
+++ src/components/base/BaseCard.tsx
@@ -1,13 +1,20 @@
 import { PropsWithChildren } from "react";
 
+function BaseCardHeader(props: PropsWithChildren) {
+  return <div className="px-4 py-5 sm:px-6">{props.children}</div>;
+}
+
+function BaseCardContent(props: PropsWithChildren) {
+  return <div className="px-4 py-5 sm:p-6">{props.children}</div>;
+}
+
 export default function BaseCard(props: PropsWithChildren) {
   return (
     <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
-      <div className="px-4 py-5 sm:px-6">
-        {/* Content goes here */}
-        {/* We use less vertical padding on card headers on desktop than on body sections */}
-      </div>
-      <div className="px-4 py-5 sm:p-6">{props.children}</div>
+      {props.children}
     </div>
   );
 }
+
+BaseCard.Header = BaseCardHeader;
+BaseCard.Content = BaseCardContent;
